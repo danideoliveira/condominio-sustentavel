e9b5ebd84e8e496a241e17dab041762764a1ee42
Danielzinhow <64176765+Danielzinhow@users.noreply.github.com>
2020-05-27 18:58:26 -0300

    initial commit

---
+++ funcoes.js
@@ -0,0 +1,16 @@
+function toggle() {
+
+	var conteudo = document.getElementById('conteudo');
+	conteudo.classList.toggle('active');
+	
+	var blur = document.getElementById('blur');
+	blur.classList.toggle('active');
+
+	var blur = document.getElementById('blur2');
+	blur2.classList.toggle('active');
+
+	var blur = document.getElementById('blur3');
+	blur3.classList.toggle('active');
+	
+
+}
